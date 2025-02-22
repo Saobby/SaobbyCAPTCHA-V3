d5f1d0e0fd6dfe6af38f9901233b32576c87578b
Saobby <101960076+Saobby@users.noreply.github.com>
2025-02-22 18:08:58 +0800

    修正健康检查脚本

--- health_checker.py
+++ health_checker.py
@@ -6,8 +6,9 @@ from config import GUNICORN_PORT
 try:
     rsp = requests.post(f"http://127.0.0.1:{GUNICORN_PORT}/api/gen_challenge", timeout=3)
     rsp = json.loads(rsp.text)
+except:
+    sys.exit(1)  # unhealthy
+else:
     if rsp["retcode"]:
         sys.exit(1)  # unhealthy
     sys.exit(0)
-except:
-    sys.exit(1)  # unhealthy
